376e2bda750b078a58c35cec14498940b91a8928
fviale <fabien.viale@activeeon.com>
2016-01-14 16:32:11 +0100

    - The generation of the start node command is now only during deployment - Replaced the "start node command" configuration with additionalProperties

--- infrastructures/rm-infrastructure-aws-ec2/src/main/java/org/ow2/proactive/resourcemanager/nodesource/infrastructure/AWSEC2Infrastructure.java
+++ infrastructures/rm-infrastructure-aws-ec2/src/main/java/org/ow2/proactive/resourcemanager/nodesource/infrastructure/AWSEC2Infrastructure.java
@@ -36,21 +36,20 @@
  */
 package org.ow2.proactive.resourcemanager.nodesource.infrastructure;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import org.apache.log4j.Logger;
 import org.objectweb.proactive.core.ProActiveException;
 import org.objectweb.proactive.core.node.Node;
 import org.ow2.proactive.resourcemanager.exception.RMException;
 import org.ow2.proactive.resourcemanager.nodesource.common.Configurable;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class AWSEC2Infrastructure extends InfrastructureManager {
 
@@ -94,8 +93,8 @@ public class AWSEC2Infrastructure extends InfrastructureManager {
 	@Configurable(description = "Command used to download the worker jar")
 	protected String downloadCommand = generateDefaultDownloadCommand();
 
-	@Configurable(description = "Start ProActive Node command")
-	protected String nodeToRMConnectionCommand = generateDefaultStartNodeCommand();
+	@Configurable(description = "Additional java command properties (e.g. \"-Dpropertyname=propertyvalue\")")
+	protected String additionalProperties = "";
 
 	@Configurable(description = "RAM (in Mega Bytes))")
 	protected int ram = 512;
@@ -131,7 +130,7 @@ public class AWSEC2Infrastructure extends InfrastructureManager {
 		this.numberOfNodesPerInstance = Integer.parseInt(parameters[8].toString().trim());
 		this.operatingSystem = parameters[9].toString().trim();
 		this.downloadCommand = parameters[10].toString().trim();
-		this.nodeToRMConnectionCommand = parameters[11].toString().trim();
+		this.additionalProperties = (parameters[11] != null )? parameters[11].toString().trim() : "";
 		this.ram = Integer.parseInt(parameters[12].toString().trim());
 		this.cpu = Integer.parseInt(parameters[13].toString().trim());
 
@@ -161,7 +160,7 @@ public class AWSEC2Infrastructure extends InfrastructureManager {
 
 		for (String instanceId : instancesId) {
 			List<String> scripts = Lists.newArrayList(this.downloadCommand,
-					"nohup " + this.nodeToRMConnectionCommand + " -DinstanceId=" + instanceId + "  &");
+					"nohup " + generateDefaultStartNodeCommand(instanceId) + "  &");
 			String instanceScriptJson = ConnectorIaasJSONTransformer.getScriptInstanceJSON(scripts);
 
 			executeScript(instanceId, instanceScriptJson);
@@ -280,7 +279,7 @@ public class AWSEC2Infrastructure extends InfrastructureManager {
 		}
 
 		if (parameters[11] == null) {
-			throw new IllegalArgumentException("The start ProActive Node command must be specified");
+			// can be empty
 		}
 
 		if (parameters[12] == null) {
@@ -298,6 +297,7 @@ public class AWSEC2Infrastructure extends InfrastructureManager {
 			// best effort, may not work for all machines
 			return InetAddress.getLocalHost().getCanonicalHostName();
 		} catch (UnknownHostException e) {
+			logger.warn(e);
 			return "localhost";
 		}
 	}
@@ -311,15 +311,16 @@ public class AWSEC2Infrastructure extends InfrastructureManager {
 		}
 	}
 
-	private String generateDefaultStartNodeCommand() {
+	private String generateDefaultStartNodeCommand(String instanceId) {
 		try {
 
 			String protocol = rmUrl.substring(0, rmUrl.indexOf(':')).trim();
 			return "java -jar node.jar -Dproactive.communication.protocol=" + protocol
-					+ " -Dproactive.pamr.router.address=" + rmDomain + " -r " + rmUrl + "-s " + nodeSource.getName()
+					+ " -Dproactive.pamr.router.address=" + rmDomain + " -DinstanceId=" + instanceId + " " + additionalProperties +" -r " + rmUrl + "-s " + nodeSource.getName()
 					+ " -w " + numberOfNodesPerInstance;
 		} catch (Exception e) {
-			return "java -jar node.jar -r " + rmUrl + "-s " + nodeSource.getName() + " -w " + numberOfNodesPerInstance;
+			logger.warn("Exception when generating the command, fallback on default value",e);
+			return "java -jar node.jar -DinstanceId=" + instanceId + " -r " + rmUrl + "-s " + nodeSource.getName() + " -w " + numberOfNodesPerInstance;
 		}
 	}
 
