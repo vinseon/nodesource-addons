f078a39f975b5b4ef2a24ab00399d7c4216a2fe0
Shatalov <yaroslav.shatalov@activeeon.com>
2016-09-12 16:18:05 +0200

    change jersey to restEasy in RestClient

--- infrastructures/infrastructure-common/src/main/java/org/ow2/proactive/resourcemanager/nodesource/infrastructure/RestClient.java
+++ infrastructures/infrastructure-common/src/main/java/org/ow2/proactive/resourcemanager/nodesource/infrastructure/RestClient.java
@@ -1,77 +1,88 @@
 package org.ow2.proactive.resourcemanager.nodesource.infrastructure;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.ClientResponse.Status;
-import com.sun.jersey.api.client.WebResource;
+import java.net.HttpURLConnection;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 
 public class RestClient {
 
-    private final Client jerseyClient;
+    private final ResteasyClient restEasyClient;
     private final String connectorIaasURL;
 
     public RestClient(String connectorIaasURL) {
-        this.jerseyClient = Client.create();
+        this.restEasyClient = new ResteasyClientBuilder().build();
         this.connectorIaasURL = connectorIaasURL;
-
     }
 
     public String getInfrastructures() {
-        WebResource infrastructuresWebResource = jerseyClient.resource(connectorIaasURL + "/infrastructures");
-        return checkResponseIsOK(
-                infrastructuresWebResource.type("application/json").get(ClientResponse.class))
-                        .getEntity(String.class);
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures");
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
+        return checkAndGetResponse(response);
     }
 
     public String getInstancesByInfrastructure(String infrastructureId) {
-        WebResource infrastructuresWebResource = jerseyClient
-                .resource(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances");
-        return checkResponseIsOK(
-                infrastructuresWebResource.type("application/json").get(ClientResponse.class))
-                        .getEntity(String.class);
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances");
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE).get();
+        return checkAndGetResponse(response);
     }
 
     public String postToInfrastructuresWebResource(String infrastructureJson) {
-        WebResource infrastructuresWebResource = jerseyClient.resource(connectorIaasURL + "/infrastructures");
-        return checkResponseIsOK(infrastructuresWebResource.type("application/json")
-                .post(ClientResponse.class, infrastructureJson)).getEntity(String.class);
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures");
+        Response response = target.request().post(Entity.entity(infrastructureJson, MediaType.APPLICATION_JSON_TYPE));
+        return checkAndGetResponse(response);
     }
 
     public void deleteInfrastructuresWebResource(String infrastructureId) {
-        WebResource infrastructuresWebResource = jerseyClient
-                .resource(connectorIaasURL + "/infrastructures/" + infrastructureId);
-        checkResponseIsOK(infrastructuresWebResource.type("application/json").delete(ClientResponse.class));
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures/" + infrastructureId);
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE).delete();
+        checkAndGetResponse(response);
     }
 
     public String postToInstancesWebResource(String infrastructureId, String instanceJson) {
-        WebResource instancesWebResource = jerseyClient
-                .resource(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances");
-        return checkResponseIsOK(
-                instancesWebResource.type("application/json").post(ClientResponse.class, instanceJson))
-                        .getEntity(String.class);
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances");
+        Response response = target.request().post(Entity.entity(instanceJson, MediaType.APPLICATION_JSON_TYPE));
+        return checkAndGetResponse(response);
     }
 
     public void deleteToInstancesWebResource(String infrastructureId, String key, String value) {
-        WebResource instancesWebResource = jerseyClient
-                .resource(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances");
-        checkResponseIsOK(instancesWebResource.queryParam(key, value).accept("application/json")
-                .delete(ClientResponse.class));
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances");
+        Response response = target.queryParam(key, value).request(MediaType.APPLICATION_JSON_TYPE).delete();
+        checkAndGetResponse(response);
     }
 
     public String postToScriptsWebResource(String infrastructureId, String key, String value,
             String scriptJson) {
-        WebResource scriptsWebResource = jerseyClient
-                .resource(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances/scripts");
-        return checkResponseIsOK(scriptsWebResource.queryParam(key, value).type("application/json")
-                .post(ClientResponse.class, scriptJson)).getEntity(String.class);
+        ResteasyWebTarget target = initWebTarget(connectorIaasURL + "/infrastructures/" + infrastructureId + "/instances/scripts");
+        Response response = target.request().post(Entity.entity(scriptJson, MediaType.APPLICATION_JSON_TYPE));
+        return checkAndGetResponse(response);
     }
 
-    private ClientResponse checkResponseIsOK(ClientResponse response) {
-        if (response.getStatus() != Status.OK.getStatusCode()) {
+    private Response checkResponseIsOK(Response response) {
+        if (response.getStatus() != HttpURLConnection.HTTP_OK) {
             throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
         }
         return response;
     }
 
+    private ResteasyWebTarget initWebTarget(String url){
+        return restEasyClient.target(url);
+    }
+
+    private String checkAndGetResponse(Response response){
+        try {
+            return checkResponseIsOK(response).readEntity(String.class);
+        }
+        finally {
+            if (response != null) {
+                response.close();
+            }
+        }
+    }
 }
